cf5d2dfe746d6bfc5fcb99813bc85cc50a841d36
Mateus Holanda <mateus23ita@gmail.com>
2023-05-12 05:21:24 -0300

    Animate progress bar on quizzes

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from 'react';
 import { View } from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming
+} from 'react-native-reanimated';
 
 import { styles } from './styles';
+
 
 interface ProgressBarProps {
   total: number;
@@ -10,9 +17,21 @@ interface ProgressBarProps {
 export function ProgressBar({ total, current }: ProgressBarProps) {
   const percentage = Math.round((current / total) * 100);
 
+  const sharedProgress = useSharedValue(percentage);
+
+  const styleAnimated = useAnimatedStyle(() => {
+    return {
+      width: `${sharedProgress.value}%`
+    }
+  });
+
+  useEffect(() => {
+    sharedProgress.value = withTiming(percentage);
+  }, [current]);
+
   return (
     <View style={styles.track}>
-      <View style={[styles.progress, { width: `${percentage}%` }]} />
+      <Animated.View style={[styles.progress, styleAnimated]} />
     </View>
   );
 }
